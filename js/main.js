f0250aa620cbbffae09e200daaec358fc0b379ae
Leejunhyeok03 <ly0609@naver.com>
2024-04-25 18:13:36 +0900

    feat:API 데이터 가져오기

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const ApiKey = '21ccf5793f9e51cfba0198fa23b3d541';
+const ApiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWNjZjU3OTNmOWU1MWNmYmEwMTk4ZmEyM2IzZDU0MSIsInN1YiI6IjY2MmEwZDFkYmYzMWYyMDA5YWUzMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sNlesGPpMfB6Nt3ZqEFMSIwcE88KWjPts2Waw_I2qp8';
+
+const ApiFetch = async (url = "/3/movie/changes?page=1") => {
+    let json;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            // Authorization: `Bearer ${ApiToken}`
+          }
+      };
+      try {
+          const response = await fetch(`https://api.themoviedb.org${url}${url.indexOf('?')?"&":"?"}api_key=${ApiKey}`, options)
+          json = await response.json();
+          console.log(json);
+      } catch (error) {
+        console.error(error);
+      }
+    return json;
+}
+
+(function init() {
+    console.log(ApiFetch());
+})()
+
