607df5f6912f3e487ef7f1b2b2c7828e0c9533a7
Leejunhyeok03 <ly0609@naver.com>
2024-04-27 05:57:59 +0900

    feat:영화 상세데이터 가져오기 및 디자인 기본틀 구성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const ApiKey = '21ccf5793f9e51cfba0198fa23b3d541';
 const ApiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWNjZjU3OTNmOWU1MWNmYmEwMTk4ZmEyM2IzZDU0MSIsInN1YiI6IjY2MmEwZDFkYmYzMWYyMDA5YWUzMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sNlesGPpMfB6Nt3ZqEFMSIwcE88KWjPts2Waw_I2qp8';
 
-const ApiFetch = async (url = "/3/movie/changes?page=1") => {
+const ApiFetch = async (url = "/3/movie/popular?language=en-US&page=1") => {
+// const ApiFetch = async (url = "/3/movie/top_rated?language=en-US&page=1") => {
     let json;
     const options = {
         method: 'GET',
@@ -10,8 +11,9 @@ const ApiFetch = async (url = "/3/movie/changes?page=1") => {
             // Authorization: `Bearer ${ApiToken}`
           }
       };
+
       try {
-          const response = await fetch(`https://api.themoviedb.org${url}${url.indexOf('?')?"&":"?"}api_key=${ApiKey}`, options)
+          const response = await fetch(`https://api.themoviedb.org${url}${ url.indexOf('?')!==-1?"&":"?" }api_key=${ApiKey}`, options)
           json = await response.json();
           console.log(json);
       } catch (error) {
@@ -20,7 +22,20 @@ const ApiFetch = async (url = "/3/movie/changes?page=1") => {
     return json;
 }
 
+const movieList = async () => {
+    const api = await ApiFetch();
+    const movies = api.results;
+    let html = "";
+    for (let i = 0; i < movies.length; i++) {
+        let movie = await ApiFetch(`/3/movie/${movies[i].id}?original_language="eu"`, );
+        console.log(movie);
+        html += `${movie.id}<br>${movie.original_title}<br> ${movie.overview}<br> <br>${movie.poster_path}${movie.vote_average}<br> <br>`
+    }
+
+    document.querySelector("#movie-list").innerHTML = html;
+}
+
 (function init() {
-    console.log(ApiFetch());
+    // movieList();
 })()
 
