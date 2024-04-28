a7a0e1078931a6e38b0aa1222c47e7dae3a8e2d9
Leejunhyeok03 <ly0609@naver.com>
2024-04-28 23:57:15 +0900

    feat: 인기순 버튼 기능 추가 및 검색 기능추가

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const ApiKey = '21ccf5793f9e51cfba0198fa23b3d541';
 const ApiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWNjZjU3OTNmOWU1MWNmYmEwMTk4ZmEyM2IzZDU0MSIsInN1YiI6IjY2MmEwZDFkYmYzMWYyMDA5YWUzMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sNlesGPpMfB6Nt3ZqEFMSIwcE88KWjPts2Waw_I2qp8';
 
-const ApiFetch = async (url = "/3/movie/popular?language=en-US&page=1") => {
-// const ApiFetch = async (url = "/3/movie/top_rated?language=en-US&page=1") => {
+const $popular = document.getElementById("popular");
+const $top_rated = document.getElementById("top_rated");
+const $form = document.getElementById('search');
+
+let movies;
+
+const ApiFetch = async (url) => {
     let json;
     const options = {
         method: 'GET',
@@ -15,27 +20,79 @@ const ApiFetch = async (url = "/3/movie/popular?language=en-US&page=1") => {
       try {
           const response = await fetch(`https://api.themoviedb.org${url}${ url.indexOf('?')!==-1?"&":"?" }api_key=${ApiKey}`, options)
           json = await response.json();
-          console.log(json);
       } catch (error) {
         console.error(error);
       }
     return json;
 }
 
-const movieList = async () => {
-    const api = await ApiFetch();
-    const movies = api.results;
+const movieListAPI = async (url = "/3/movie/popular?language=en-US&page=1") => {
+    const api = await ApiFetch(url);
+    movies = api.results;
+    createMovieList(movies);
+}
+
+
+const createMovieList = async (movies)  => {
     let html = "";
     for (let i = 0; i < movies.length; i++) {
-        let movie = await ApiFetch(`/3/movie/${movies[i].id}?original_language="eu"`, );
-        console.log(movie);
-        html += `${movie.id}<br>${movie.original_title}<br> ${movie.overview}<br> <br>${movie.poster_path}${movie.vote_average}<br> <br>`
+        let movie = await ApiFetch(`/3/movie/${movies[i].id}?original_language="eu-US"`, );
+        html += `
+        <div class="movie ${movie.id}" onClick="alert('영화 id: ${movie.id}')">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.id}">
+            <div class="hover">
+                <h3 class="title bold">
+                ${movie.title}
+                </h3>
+                <p>
+                ${movie.overview}
+                </p>
+                <span>평점: ${movie.vote_average}</span>
+            </div>
+        </div>
+        `
     }
 
     document.querySelector("#movie-list").innerHTML = html;
 }
 
+const handleSearch = (e) => {
+    e.preventDefault();
+    let movieSearch = movies.filter((movie) => movie.title.toLowerCase().includes(e.target[0].value.toLowerCase()));
+    console.log(movieSearch);
+    createMovieList(movieSearch);
+}
+
+
 (function init() {
-    // movieList();
+    movieListAPI();
 })()
+
+
+window.onload = function(){
+
+
+    $top_rated.addEventListener('click', (e) => {
+        if(!e.target.classList.contains('chk')){
+            let $chk = document.getElementsByClassName("chk")[0];
+            movieListAPI("/3/movie/top_rated?language=en-US&page=1");
+            $chk.classList.remove("chk");
+            e.target.className += 'chk';
+        }
+    });
+    
+    $popular.addEventListener('click', (e) => {
+        if(!e.target.classList.contains('chk')){
+            let $chk = document.getElementsByClassName("chk")[0];
+            movieListAPI();
+            $chk.classList.remove("chk");
+            e.target.className += 'chk';
+        }
+    });
+
+
+
+}
+
+
 
