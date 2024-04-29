f38449ea945942225a091f19f3af1e7ca1ddf580
Leejunhyeok03 <ly0609@naver.com>
2024-04-29 20:47:31 +0900

    fix: js구분 및 script type='module'추가

--- js/main.js
+++ js/main.js
@@ -1,74 +1,16 @@
-const ApiKey = '21ccf5793f9e51cfba0198fa23b3d541';
-const ApiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWNjZjU3OTNmOWU1MWNmYmEwMTk4ZmEyM2IzZDU0MSIsInN1YiI6IjY2MmEwZDFkYmYzMWYyMDA5YWUzMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sNlesGPpMfB6Nt3ZqEFMSIwcE88KWjPts2Waw_I2qp8';
+import { movieListAPI, handleSearch } from "./movie.js"
 
-const $form = document.getElementById('search');
 const $pageUp = document.getElementById('pageUp');
 const $menu_btn = document.querySelectorAll('.menu_btn');
 
-let movies, scrollHeight;
-
-const ApiFetch = async (url) => {
-    let json;
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            // Authorization: `Bearer ${ApiToken}`
-          }
-      };
-
-      try {
-          const response = await fetch(`https://api.themoviedb.org${url}${ url.indexOf('?')!==-1?"&":"?" }api_key=${ApiKey}`, options)
-          json = await response.json();
-      } catch (error) {
-        console.error(error);
-      }
-    return json;
-}
-
-const movieListAPI = async (url = "/3/movie/popular?language=en-US&page=1") => {
-    const api = await ApiFetch(url);
-    movies = api.results;
-    console.log(api.results);
-    await createMovieList(movies);
-} 
-
-
-const createMovieList = async (movies)  => {
-    document.querySelector("#movie-list").innerHTML = movies.map((e) =>
-        `
-        <div class="movie ${e.id}" onClick="alert('영화 id: ${e.id}')">
-            <img src="https://image.tmdb.org/t/p/w500${e.poster_path}" alt="${e.id}">
-            <div class="hover">
-                <h3 class="title bold">
-                ${e.title}
-                </h3>
-                <p>
-                ${e.overview}
-                </p>
-                <span>평점: ${e.vote_average}</span>
-            </div>
-        </div>
-        `
-    ).join("");
-
-
-}
-
-const handleSearch = (e) => {
-    e.preventDefault();
-    let movieSearch = movies.filter((movie) => movie.title.toLowerCase().includes(e.target[0].value.toLowerCase()));
-    createMovieList(movieSearch);
-}
-
-
+let scrollHeight;
 
 addEventListener('scroll', (e) => {
     scrollHeight = e.target.documentElement.scrollTop;
     sessionStorage.setItem("scrollY", scrollHeight);
 });
 
-
+document.getElementById('search').addEventListener('submit', handleSearch);
 
 $menu_btn.forEach(btn => {
     btn.addEventListener('click', (e) => {
@@ -84,8 +26,6 @@ $menu_btn.forEach(btn => {
         }
     });
 });
-
-
 
 $pageUp.addEventListener('click', (e) => {
     window.scrollTo({ top: 0, behavior: "smooth" });
---
+++ js/movie.js
@@ -0,0 +1,59 @@
+const ApiKey = '21ccf5793f9e51cfba0198fa23b3d541';
+const ApiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWNjZjU3OTNmOWU1MWNmYmEwMTk4ZmEyM2IzZDU0MSIsInN1YiI6IjY2MmEwZDFkYmYzMWYyMDA5YWUzMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sNlesGPpMfB6Nt3ZqEFMSIwcE88KWjPts2Waw_I2qp8';
+
+
+let movies;
+
+const ApiFetch = async (url) => {
+    let json;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            // Authorization: `Bearer ${ApiToken}`
+          }
+      };
+
+      try {
+          const response = await fetch(`https://api.themoviedb.org${url}${ url.indexOf('?')!==-1?"&":"?" }api_key=${ApiKey}`, options)
+          json = await response.json();
+      } catch (error) {
+        console.error(error);
+      }
+    return json;
+}
+
+export const handleSearch = (e) => {
+    e.preventDefault();
+    let movieSearch = movies.filter((movie) => movie.title.toLowerCase().includes(e.target[0].value.toLowerCase()));
+    createMovieList(movieSearch);
+}
+
+export const movieListAPI = async (url = "/3/movie/popular?language=en-US&page=1") => {
+    const api = await ApiFetch(url);
+    movies = api.results;
+    console.log(api.results);
+    await createMovieList(movies);
+} 
+
+
+
+
+const createMovieList = async (movies)  => {
+    document.querySelector("#movie-list").innerHTML = movies.map((e) =>
+        `
+        <div class="movie ${e.id}" onClick="alert('영화 id: ${e.id}')">
+            <img src="https://image.tmdb.org/t/p/w500${e.poster_path}" alt="${e.id}">
+            <div class="hover">
+                <h3 class="title bold">
+                ${e.title}
+                </h3>
+                <p>
+                ${e.overview}
+                </p>
+                <span>평점: ${e.vote_average}</span>
+            </div>
+        </div>
+        `
+    ).join("");
+}
